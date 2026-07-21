11f0398b3ee290580a290dddc6618ccf2cde499f
Hélio Marcos <h.paixao@aluno.senai.br>
2026-07-21 09:49:23 -0300

    Faltando Ajustes para Funcionar

---
+++ js/login.ts
@@ -0,0 +1,25 @@
+// Adicione esta linha no final do cadastrar.ts para "liberar" a função para o HTML
+(window as any).cadastrar= cadastrar;
+
+function fazerLogin(): void {
+    const inputUsuario = document.getElementById('usuario') as HTMLInputElement;
+    const inputSenha = document.getElementById('senha') as HTMLInputElement;
+
+    const usuario: string = inputUsuario.value;
+    const senha: string = inputSenha.value;
+
+    if (usuario === '' || senha === '') {
+        alert("Preencha todos os campos de login!");
+        return;
+    }
+
+    // Validação simples (Simulação de login)
+    if (usuario === 'admin' && senha === '1234') {
+        alert("Login realizado com sucesso!");
+        window.location.href = "index.html"; // Redireciona de volta para a Home
+    } else {
+        alert("Usuário ou senha incorretos! (Dica: tente admin / 1234)");
+    }
+
+} 
+(window as any).fazerLogin = fazerLogin;
--- js/produtos.ts
+++ js/produtos.ts
@@ -1,13 +1,11 @@
-// Precisamos da mesma interface aqui para o TypeScript saber como ler a lista
 interface Produto {
     nome: string;
     preco: string;
 }
 
 function mostrarNaTela(): void {
-    // Avisamos ao TypeScript que isso é uma lista HTML (HTMLUListElement)
     const lista = document.getElementById('lista') as HTMLUListElement;
-    lista.innerHTML = ''; 
+    lista.innerHTML = '';
 
     const produtosSalvos: string | null = localStorage.getItem('meusProdutos');
     let gavetaDeProdutos: Produto[] = [];
@@ -18,7 +16,7 @@ function mostrarNaTela(): void {
 
     if (gavetaDeProdutos.length === 0) {
         lista.innerHTML = '<li>Nenhum produto cadastrado ainda.</li>';
-        return; 
+        return;
     }
 
     for (let i = 0; i < gavetaDeProdutos.length; i++) {
@@ -27,3 +25,6 @@ function mostrarNaTela(): void {
 }
 
 mostrarNaTela();
+
+// Adicione esta linha no final do cadastrar.ts para "liberar" a função para o HTML
+(window as any).cadastrar = cadastrar;
